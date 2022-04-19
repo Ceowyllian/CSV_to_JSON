fd4da2c59eb8bef1c222ff3dc76fb0389cca78f9
Ceowyllian <downwell@online.ua>
2022-04-19 20:34:58 +0300

    Refactor

--- mapping.py
+++ mapping.py
@@ -1,23 +1,81 @@
 class Mapping:
+    """
+    Конструктор принимает схему-словарь для связи csv строк
+    и json объектов.
+
+    Каждая пара "ключ-значение" в схеме описывает связь между
+    полем JSON-объекта и значением/значениями из таблицы.
+
+    Ключ словаря определяет название поля в JSON-объекте.
+    Допустимые ключи:
+     *  **строка** - название одновременно для поля JSON-объекта и
+        для связанного столбца в таблице.
+     *  **кортеж** - должен содержать две строки: первая - название
+        столбца таблицы, вторая - тип данных.
+
+    Значением (**value**) ключа словаря может быть **строка**, **список**,
+    **словарь** и **кортеж**.
+
+    **value**-**строка** (допустимый ключ - **строка**)
+    Если **value** - строка, то она определяет тип, к которому нужно
+    преобразовать данные из столбца таблицы.
+
+    **value**-**список** (допустимый ключ - **строка**)
+    Может быть двух видов:
+     *  ['**int**'] - список содержит единственный элемент-строку,
+        которая определяет тип данных значений списка. Значения
+        должны быть перечислены в ячейке таблицы через запятую.
+     *  [('name', '**str**'), ('age', '**int**')] - список содержит один
+        или несколько кортежей (название столбца, тип данных).
+        Данные будут взяты из соответствующих столбцов таблицы,
+        преобразованы к указанным типам и помещены в список.
+
+    **value**-**кортеж** (допустимый ключ - **кортеж**)
+    Тоже должен содержать 2 строки - название столбца и тип
+    данных. Пара "ключ-кортеж: строка-кортеж" связывает два
+    столбца в таблице. Полем JSON-объекта будет значение из
+    первого столбца таблицы, а значением JSON-объекта - значение
+    из второго столбца.
+
+    **value**-**словарь** (допустимый ключ - **строка**)
+    Служит для описания вложенного JSON-объекта. Ключи и значения
+    такого словаря должны соответствовать условиям, описанным выше.
+
+    """
 
     def __init__(self, schema: dict):
-        self.__validate_structure(schema)
+        self.__validate_schema(schema)
         fields = self.__extract_fields(schema)
         if len(fields) != len(set(fields)):
             raise ValueError('Заголовок таблицы не может содержать одинаковые поля!')
-        self.__struct = schema
+        self.__schema = schema
         self.__table_fields = fields
 
-    def __validate_structure(self, obj: dict):
-        if not isinstance(obj, dict):
+    def __validate_schema(self, schema: dict):
+        if not isinstance(schema, dict):
             raise TypeError('Неверный тип, ожидался словарь')
 
-        for k, v in zip(obj.keys(), obj.values()):
-            if not (isinstance(v, tuple) and isinstance(k, tuple)):
-                if isinstance(v, tuple) or isinstance(k, tuple):
-                    raise ValueError('Ключ и значение должны быть кортежами одновременно')
-            self.__validate_key(k)
+        for k, v in zip(schema.keys(), schema.values()):
+            if isinstance(k, tuple) or isinstance(v, tuple):
+                self.__validate_tuple(k)
+                self.__validate_tuple(v)
+                continue
+            self.__validate_name(k)
             self.__validate_value(v)
+
+    def __validate_value(self, v):
+        {
+            str: self.__validate_type,
+            list: self.__validate_list,
+            dict: self.__validate_schema,
+        }[type(v)](v)
+
+    @staticmethod
+    def __validate_name(s: str):
+        if not isinstance(s, str):
+            raise TypeError('Название столбца должно быть строкой')
+        if s == '' or s.isspace() or not s.isprintable():
+            raise ValueError('Недопустимое название столбца')
 
     @staticmethod
     def __validate_type(s: str):
@@ -26,12 +84,12 @@ class Mapping:
         if s not in ('int', 'float', 'str', 'bool'):
             raise ValueError('Недопустимое название типа')
 
-    @staticmethod
-    def __validate_name(s: str):
-        if not isinstance(s, str):
-            raise TypeError('Название столбца должно быть строкой')
-        if s == '' or s.isspace() or not s.isprintable():
-            raise ValueError('Недопустимое название столбца')
+    def __validate_tuple(self, t: tuple):
+        if not isinstance(t, tuple):
+            raise TypeError('Неверный тип, ожидался кортеж')
+        name, type_of = t
+        self.__validate_name(name)
+        self.__validate_type(type_of)
 
     def __validate_list(self, lst: list):
         if not isinstance(lst, list):
@@ -45,74 +103,58 @@ class Mapping:
             for tpl in lst:
                 self.__validate_tuple(tpl)
 
-    def __validate_tuple(self, t: tuple):
-        if not isinstance(t, tuple):
-            raise TypeError('Неверный тип, ожидался кортеж')
-        name, type_of = t
-        self.__validate_name(name)
-        self.__validate_type(type_of)
+    def __convert(self, to: str, val):
 
-    def __validate_key(self, k):
-        if isinstance(k, str):
-            self.__validate_name(k)
-            return
-        if isinstance(k, tuple):
-            name, type_of = k
-            self.__validate_name(name)
-            self.__validate_type(type_of)
-            return
-        raise TypeError('Неверный тип, ожидалась строка или кортеж')
+        def to_bool(x):
+            try:
+                return int(x) != 0
+            except ValueError:
+                return False
 
-    def __validate_value(self, v):
-        {
-            str: self.__validate_type,
-            list: self.__validate_list,
-            tuple: self.__validate_tuple,
-            dict: self.__validate_structure,
-        }[type(v)](v)
+        self.__validate_type(to)
+        return {
+            'int': lambda x: int(x),
+            'float': lambda x: float(x),
+            'str': lambda x: str(x),
+            'bool': lambda x: to_bool(x)
+        }[to](val)
 
-    def __extract_fields(self, mapping: dict):
+    def __extract_fields(self, schema: dict):
         fields = []
-        for k, v in zip(mapping.keys(),
-                        mapping.values()):
-            if isinstance(k, str) and isinstance(v, str):
-                fields.append(k)
-                continue
-            if isinstance(k, str) and isinstance(v, dict):
-                fields.extend(self.__extract_fields(v))
-                continue
+        for k, v in zip(schema.keys(),
+                        schema.values()):
+
             if isinstance(k, tuple) and isinstance(v, tuple):
                 fields.append(k[0])
                 fields.append(v[0])
                 continue
-            if isinstance(k, str) and isinstance(v, list):
+
+            if isinstance(v, str):
+                fields.append(k)
+                continue
+
+            if isinstance(v, dict):
+                fields.extend(self.__extract_fields(v))
+                continue
+
+            if isinstance(v, list):
                 if len(v) == 1 and isinstance(v[0], str):
                     fields.append(k)
                     continue
-                elif all([isinstance(x, tuple) for x in v]):
-                    fields.extend([field[0] for field in v])
-                    continue
+                fields.extend([field[0] for field in v])
+                continue
 
         return fields
 
+    @property
     def table_fields(self):
         return self.__table_fields
 
     def row_to_dict(self, row: dict):
-        fields = list(row.keys())
-        if set(self.__table_fields) != set(fields):
+        fields = set(list(row.keys()))
+        if set(self.__table_fields) != fields:
             raise ValueError('Поля не совпадают!')
-        struct = self.__create_structure(self.__struct, row)
-        return struct
-
-    @staticmethod
-    def __convert(to: str, val):
-        return {
-                'int': lambda x: int(x),
-                'float': lambda x: float(str(x).replace(',', '.')),
-                'str': lambda x: str(x),
-                'bool': lambda x: bool(x)
-                }[to](val)
+        return self.__create_structure(self.__schema, row)
 
     def __create_structure(self, schema: dict,  row: dict):
         struct = dict()
--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from mapping import Mapping
 def create_table(path: str, schema: dict):
     mapping = Mapping(schema)
     with open(path, 'w') as csvfile:
-        header = mapping.table_fields()
+        header = mapping.table_fields
         writer = csv.DictWriter(csvfile, header, delimiter=';')
         writer.writeheader()
 
@@ -15,7 +15,7 @@ def csv_to_json(csvpath: str, jsonpath: str, schema: dict):
     mapping = Mapping(schema)
     with open(csvpath, 'r') as csvfile:
         with open(jsonpath, 'w') as jsonfile:
-            header = mapping.table_fields()
+            header = mapping.table_fields
             csvreader = csv.DictReader(csvfile, header, delimiter=';')
             strbuffer = '['
             for row in csvreader:
